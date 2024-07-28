8c1d34599fe8d0ed3fada29f4c457c3c535ce92a
Radha Wagh <145145559+RadhaWagh@users.noreply.github.com>
2024-07-28 18:22:48 +0530

    Add files via upload

---
+++ 4.SwitchCase.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x=1;
+    
+    switch(x)
+    {
+        case 1: cout<<"one";
+        break;
+        case 2: cout<<"two";
+        break;
+        case 3: cout<<"three";
+        break;
+        default : cout<<"invalid number";
+        break;
+    }
+    cout<<endl<<endl;
+    return 0;
+}
