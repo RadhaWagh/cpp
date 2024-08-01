dc6c86e061bc29d0786a2463c4c73ae601552c10
Radha Wagh <145145559+RadhaWagh@users.noreply.github.com>
2024-08-01 23:18:35 +0530

    Add files via upload

---
+++ 11.MultiplicationTable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cout<<"Enter a Number";
+    cin>>n;
+    
+    for(i=1;i<=10;i++)
+    {
+        cout<<n<<" X "<<i<<" = "<<i*n<<endl;
+    }
+    return 0;
+}
