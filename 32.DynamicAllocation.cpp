6cfc742287dcfe75106f6a5ffc497b5005c3e86e
Radha Wagh <145145559+RadhaWagh@users.noreply.github.com>
2024-08-19 22:56:52 +0530

    Add files via upload

---
+++ 32.DynamicAllocation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout<<"Enter Number Of Elements";
+    cin>>size;
+    int A[size];
+    
+    cout<<sizeof A<<endl;
+    
+    return 0;
+}
