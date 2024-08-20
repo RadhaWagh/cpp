9bd97741cc4f0fe37eb33e2569e743a86f160d88
Radha Wagh <145145559+RadhaWagh@users.noreply.github.com>
2024-08-20 22:52:19 +0530

    Add files via upload

---
+++ 33String.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char s[20];
+    cout<<"Enter your name";
+    cin.get(s,20);
+    cout<<"Welcome "<<s<<endl;
+    return 0;
+    
+}
