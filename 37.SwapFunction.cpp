3b4eab113109657ce19127fd9feab30ed6832825
Radha Wagh <145145559+RadhaWagh@users.noreply.github.com>
2024-08-22 20:42:34 +0530

    Add files via upload

---
+++ 37.SwapFunction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str1="programminng";
+    string str2="language";
+    str1.swap(str2);
+
+    cout<<str1<<endl;
+    return 0;
+    
+}
